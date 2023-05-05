da0bcdfaa93ac982f427311c2699ef86e535d52f
remolacho <jonathang_25@gmail.com>
2023-05-05 14:16:03 -0500

    It was added repository tweet mongo and mock

---
+++ infraestructure/repositories/mongoDB/tweet.repository/create.go
@@ -0,0 +1,22 @@
+package tweet_repository
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+	"twittor-api/domain/models/tweet"
+)
+
+func (r *TweetRepository) Create(t *tweet.Tweet) (*tweet.Tweet, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+
+	var err error
+
+	defer cancel()
+
+	t.ID = primitive.NewObjectID()
+
+	_, err = r.Tweet.InsertOne(ctx, t)
+
+	return t, err
+}
