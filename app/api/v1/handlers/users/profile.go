cce41eeac2132dbddbed5228a821dbab05e00fd3
remolacho <jonathang_25@gmail.com>
2023-06-11 15:28:40 -0500

    It fix EP profile

--- app/api/v1/handlers/users/profile.go
+++ app/api/v1/handlers/users/profile.go
@@ -4,16 +4,23 @@ import (
 	"encoding/json"
 	"net/http"
 	"twittor-api/app/middleware"
+	responseService "twittor-api/app/services/response.service"
 	userService "twittor-api/app/services/user.service"
 	repositoryFactoryUser "twittor-api/infraestructure/repositories/factories/repository.factory.user"
 )
 
-// Profile GET route /v1/users/profile
+// Profile GET route /v1/users/profile?userId
 func Profile(w http.ResponseWriter, r *http.Request) {
+	userId := r.URL.Query().Get("userId")
 	claim, _ := middleware.AuthClaim(r)
 	repositoryUser := repositoryFactoryUser.Build()
 	profile := userService.NewProfile(repositoryUser)
-	response, err := profile.Get(claim.ID.Hex())
+
+	if len(userId) == 0 {
+		userId = claim.ID.Hex()
+	}
+
+	response, err := profile.Get(userId)
 
 	if err != nil {
 		http.Error(w, "User not found", http.StatusNotFound)
@@ -22,5 +29,5 @@ func Profile(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(responseService.Call(true, "", response))
 }
