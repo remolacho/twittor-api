462734be4e0cca4c0131bf515c30036fb9df11f7
remolacho <jonathang_25@gmail.com>
2023-06-11 15:28:40 -0500

    It fix EP profile

--- app/services/user.service/profile.go
+++ app/services/user.service/profile.go
@@ -1,8 +1,25 @@
 package user_service
 
 import (
+	"os"
 	"twittor-api/domain/models/user"
 )
+
+type Banner struct {
+	BannerUrl    string `json:"bannerUrl"`
+	AlternateUrl string `json:"alternateUrl"`
+}
+
+type Avatar struct {
+	AvatarUrl    string `json:"avatarUrl"`
+	AlternateUrl string `json:"alternateUrl"`
+}
+
+type ResponseProfile struct {
+	*user.User `json:"user"`
+	Avatar     `json:"avatar"`
+	Banner     `json:"banner"`
+}
 
 type UserProfileService struct {
 	RepositoryUser user.IUser
@@ -14,7 +31,7 @@ func NewProfile(repository user.IUser) *UserProfileService {
 	}
 }
 
-func (s *UserProfileService) Get(ID string) (*user.User, error) {
+func (s *UserProfileService) Get(ID string) (*ResponseProfile, error) {
 	u, err := s.RepositoryUser.Find(ID)
 
 	if err != nil {
@@ -23,5 +40,21 @@ func (s *UserProfileService) Get(ID string) (*user.User, error) {
 
 	u.Password = ""
 
-	return u, nil
+	response := &ResponseProfile{
+		User: u,
+		Avatar: Avatar{
+			AvatarUrl:    u.Avatar,
+			AlternateUrl: os.Getenv("AVATAR_ALT"),
+		},
+		Banner: Banner{
+			BannerUrl:    u.Banner,
+			AlternateUrl: os.Getenv("BANNER_ALT"),
+		},
+	}
+
+	u.Password = ""
+	u.Banner = ""
+	u.Avatar = ""
+
+	return response, nil
 }
